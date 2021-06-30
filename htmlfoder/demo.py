9e5c4bbf1c9862c92c8b0ca1da74912b7f1e7fbe
Megha Sajeev <meghasajeev14@gmail.com>
2021-07-01 01:22:33 +0530

    front end files

---
+++ htmlfoder/demo.py
@@ -0,0 +1,11 @@
+#html---to display contents
+
+#to open vscode
+# go to location in C---front end--edit address---type cmd---enter---on screen type --code<space>.
+
+#
+# to get default syntax in html --- shift+!
+
+#3 sections--header--main--footer--in html page
+
+#alt+w to wrap
